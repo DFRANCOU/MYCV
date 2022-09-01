a3f1a27804bd4ebbd002de32700c130a13250ae3
DFRANCOU <davidf.frncou@gmail.com>
2022-09-01 14:21:09 -0500

    cambio en css dark

--- proyectcv.js
+++ proyectcv.js
@@ -1,49 +1,47 @@
 
+document.querySelectorAll(".tileSmlie")
+console.log(document.querySelectorAll(".tileSmlie"));
 
-    let x = document.querySelector(".contactSmile")
 
-    console.log("x");
+const button1 = document.querySelector(".button1");
 
-// const button1 = document.querySelector(".button1");
+button1.addEventListener("click", lightMode);
 
-// button1.addEventListener("click", lightMode);
+function lightMode(){
 
-// function lightMode(){
+    document.querySelector("body").classList.toggle("change-mode-body");
 
-//     document.querySelector("body").classList.toggle("change-mode-body");
+    document.querySelectorAll(".a-header").forEach(element => { 
+    element.classList.toggle("change-mode-a-header")
+    });
 
-//     document.querySelectorAll(".a-header").forEach(element => { 
-//     element.classList.toggle("change-mode-a-header")
-//     });
+    document.querySelectorAll(".cajaGrande").forEach(element => {
+    element.classList.toggle("change-mode-cajaGrande")
+    });
 
-//     document.querySelectorAll(".cajaGrande").forEach(element => {
-//     element.classList.toggle("change-mode-cajaGrande")
-//     });
+    document.querySelectorAll("div h3").forEach(element => {
+    element.classList.toggle("change-mode-h3")
+    });
 
-//     document.querySelectorAll("div h3").forEach(element => {
-//     element.classList.toggle("change-mode-h3")
-//     });
-
-//     document.querySelectorAll("h4").forEach(element => {
-//     element.classList.toggle("change-mode-h4") 
-//     });
-
-   
-
-//     document.querySelector(".contactSmile").classList.toggle("change-mode-contactSmile");
+    document.querySelectorAll("h4").forEach(element => {
+    element.classList.toggle("change-mode-h4") 
+    });
     
+    let startJoke = document.querySelector(".startJoke");
+      
+       startJoke.classList.toggle("change-mode-startJoke");
 
+    // document.querySelector(".startJoke").classList.toggle("change-mode-startJoke");
     
+    document.querySelector(".jokeBox").classList.toggle("change-mode-jokeBox");
 
-//     document.querySelector(".cajagrandeSmile").classList.toggle("change-mode-cajagrandeSmile");
+    document.querySelector(".logowwcm").classList.toggle("change-mode-lwwcm");
 
-//     // document,getElementsByClassName("logowwcm").classList.toggle("change-mode-lwwcm");
+    // document.querySelector(".a-here").classList.toggle("change-mode-a-here:link");
 
-//     // document.querySelector(".a-here").classList.toggle("change-mode-a-here:link");
-
-//     // document.querySelector(".a-letsGo").classList.toggle(".change-mode-a-letsGo");
+    // document.querySelector(".a-letsGo").classList.toggle(".change-mode-a-letsGo");
 
     
   
 
-// }
+}
